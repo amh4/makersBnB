3c9e80c6b9e415446eff88366a960b767c5bb45e
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-16 13:59:23 +0000

    Project initalisation

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '3.0.2'
+
+# gem "rails"
+
+gem 'rack-test', '~> 2.0'
+gem 'rake', '~> 13.0'
+gem 'sinatra', '~> 3.0'
+gem 'sinatra-activerecord', '~> 2.0'
+gem 'sinatra-contrib', '~> 3.0'
+gem 'webrick', '~> 1.7'
+
+gem 'rubocop', require: false
---
+++ app.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require_relative 'lib/booking'
+require_relative 'lib/property'
+require_relative 'lib/user'
+
+class MakersBnB < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
