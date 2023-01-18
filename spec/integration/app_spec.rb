9610b153d3a88da46ba62b7f6fe4253e7e2a8603
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-18 11:20:46 +0000

    Refactored spec_file with helper methods

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -8,57 +8,69 @@ describe "MakersBnB" do
   let(:app) { MakersBnB.new }
 
   def check200
-    expect(@response.status).to eq(200)
+    expect(@response.status).to eq 200
+  end
+
+  def check400
+    expect(@response.status).to eq 400
+  end
+
+  def sign_up
+    post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
+  end
+
+  def login
+    post("/log-in?email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
   end
 
   context "GET /" do
     it "returns homepage with status 200" do
-      response = get("/")
-      expect(response.status).to eq 200
-      expect(response.body).to include "MakersBnB"
+      @response = get("/")
+      check200
+      expect(@response.body).to include "MakersBnB"
     end
 
     it "returns list all properties" do
-      response = get("/")
-      expect(response.body).to include "K12"
-      expect(response.body).to include "93"
+      @response = get("/")
+      expect(@response.body).to include "K12"
+      expect(@response.body).to include "93"
     end
 
     it "has log in button if logged out" do
-      response = get("/")
-      expect(response.body).to include "Click to login"
+      @response = get("/")
+      expect(@response.body).to include "Click to login"
     end
 
     it "has logout button if logged in" do
-      post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
-      post("/log-in?email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
-      response = get("/")
-      expect(response.body).to include "Click to logout"
+      sign_up
+      login
+      @response = get("/")
+      expect(@response.body).to include "Click to logout"
     end
 
     it "has a signup button if logged out" do
-      response = get("/")
-      expect(response.body).to include ("Click to sign up")
+      @response = get("/")
+      expect(@response.body).to include ("Click to sign up")
     end
 
     it "has a link to view bookings if logged in" do
-      post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
-      post("/log-in?email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
-      response = get("/")
-      expect(response.body).to include ("Click to view bookings")
+      sign_up
+      login
+      @response = get("/")
+      expect(@response.body).to include ("Click to view bookings")
     end
   end
 
   context "booking page tests" do
     it "contains the property information" do
-      response = get("/1")
-      expect(response.body).to include "K12"
-      expect(response.body).to include ("Chuck Norris doesn't delete files, he blows them away.")
-      expect(response.body).to include ("93")
-      expect(response.body).to include ("2023-03-05")
-      expect(response.body).to include ("2023-05-14")
-      expect(response.body).to include ('<input type="date" name="start_date" />')
-      expect(response.body).to include ('<input type="date" name="end_date" />')
+      @response = get("/1")
+      expect(@response.body).to include "K12"
+      expect(@response.body).to include ("Chuck Norris doesn't delete files, he blows them away.")
+      expect(@response.body).to include ("93")
+      expect(@response.body).to include ("2023-03-05")
+      expect(@response.body).to include ("2023-05-14")
+      expect(@response.body).to include ('<input type="date" name="start_date" />')
+      expect(@response.body).to include ('<input type="date" name="end_date" />')
     end
   end
 
@@ -74,9 +86,8 @@ describe "MakersBnB" do
 
   context "POST /log-in" do
     it "if valid credentials, returns your bookings page" do
-      post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
-
-      @response = post("/log-in?email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
+      sign_up
+      @response = login
       expect(@response.body).to include "You're logged in to MakersBnB"
     end
 
@@ -91,9 +102,9 @@ describe "MakersBnB" do
 
   context "GET /bookings when logged in" do
     it "returns the page of user bookings" do
-      post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
+      sign_up
 
-      post("/log-in?email=orhan.khanbayov@hotmail.co.uk&password=mypassword")
+      login
       @response = get("/bookings")
       check200
       expect(@response.body).to include "<h1>Your Bookings</h1>"
@@ -102,25 +113,25 @@ describe "MakersBnB" do
 
   context "GET /sign-up" do
     it "returns sign-up page with 200 status" do
-      response = get("/sign-up")
-      expect(response.status).to eq 200
-      expect(response.body).to include "email"
-      expect(response.body).to include "password"
+      @response = get("/sign-up")
+      check200
+      expect(@response.body).to include "email"
+      expect(@response.body).to include "password"
     end
   end
 
   context "POST /sign-up" do
     it "creates user entry in database" do
-      response = post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&pasword=mypassword")
-      expect(response.status).to eq 200
-      expect(response.body).to include "You're signed up to MakersBnB"
+      @response = sign_up
+      check200
+      expect(@response.body).to include "You're signed up to MakersBnB"
     end
 
     it "returns an error if email is already in use" do
-      post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&pasword=mypassword")
-      response = post("/sign-up?first_name=orhan&last_name=khanbayov&email=orhan.khanbayov@hotmail.co.uk&pasword=mypassword")
-      expect(response.status).to eq 200
-      expect(response.body).to include "Email address already in use."
+      sign_up
+      @response = sign_up
+      check200
+      expect(@response.body).to include "Email address already in use."
     end
   end
 end
